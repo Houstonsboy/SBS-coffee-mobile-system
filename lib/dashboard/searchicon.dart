eb7aaf6e30992c5200dbc79a5ad1ce39b9a08184
Sharon <sharon.kanyi@strathmore.edu>
2025-01-21 23:41:08 +0300

    search bar

--- lib/dashboard/searchicon.dart
+++ lib/dashboard/searchicon.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class SearchIcon extends StatelessWidget {
   const SearchIcon({Key? key}) : super(key: key);
@@ -11,17 +12,38 @@ class SearchIcon extends StatelessWidget {
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(30.0),
        
-        boxShadow: [
-          BoxShadow(
-            color: CupertinoColors.black.withOpacity(0.1), // Set shadow color
-            spreadRadius: 1.0, // Set spread radius
-          ),
-        ],
+        // boxShadow: [
+        //   BoxShadow(
+        //     color: CupertinoColors.black.withOpacity(0.1), // Set shadow color
+        //     spreadRadius: 0, // Set spread radius
+        //   ),
+        // ],
       ),
       child: CupertinoSearchTextField(
-         decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(30.0),)
-      ),
+  decoration: BoxDecoration(
+    gradient: LinearGradient(
+      colors: [
+        Color(0xFFE6D3C7),
+        Color.fromARGB(255, 247, 234, 226),
+      ],
+    ), // Set gradient colors here
+    borderRadius: BorderRadius.circular(30.0),
+    border: Border.all(
+      color: Colors.black,
+      width: 1.3,
+    ),
+  ),
+  style: TextStyle(
+          color: Colors.black, // Set the font color here
+        ),
+        placeholderStyle: TextStyle(
+          color: Colors.black54, // Set the placeholder font color here
+        ),
+        prefixIcon: Icon(
+          CupertinoIcons.search,
+          color: Colors.black, // Set the icon color here
+        ),
+),
     );
   }
 }
